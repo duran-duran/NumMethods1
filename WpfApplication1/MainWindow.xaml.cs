858f28b46286c665f80e4b3c9589f8a0f7726c3e
duran-duran <bender1511@gmail.com>
2014-11-16 21:28:14 +0300

    Separate file for Matrix class was created

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Matrix JacobiMethod(Matrix A, Matrix b, double e, int maxN)
+        {
+            Matrix x = new Matrix(A.rows, 1);
+            Matrix E = new Matrix(A.rows, A.cols); E.ToIdentityMatrix();
+
+            Matrix tmp = A.Copy();
+            A = A.Transpose() * A;
+            b = tmp.Transpose() * b;
+            tmp = b.Copy();
+            double t = 2 / (0.79 * A.Norm());
+
+            int i;
+            for (i = 0; i < maxN; i++)
+            {               
+                x = (E - t*A) * tmp + t*b;
+                if ((x - tmp).Norm() < e)
+                    break;
+                tmp = x.Copy();
+            }
+
+            return x;
+        }
+
+
+        public Matrix UpperRelax(Matrix A, Matrix b, double e, int maxN)
+        {
+            Matrix x = new Matrix(A.rows, 1);
+            Matrix E = new Matrix(A.rows, A.cols); E.ToIdentityMatrix();
+
+            Matrix tmp = A.Copy();
+            A = A.Transpose() * A;
+            b = tmp.Transpose() * b;
+            Matrix M = A.ToLowTr();
+            Matrix MInv = LowTrInverse(M);
+            tmp = x.Copy();
+            double t = 1;
+
+            int i;
+            for (i = 0; i < maxN; i++)
+            {
+                x = (E - t * MInv * A) * tmp + t * MInv * b;
+                if ((x - tmp).Norm() < e)
+                    break;
+                tmp = x.Copy();
+            }
+
+            return x;
+        }
+
+
+        //Обращение нижнетреугольной матрицы. Сделать проверку?
+        public Matrix LowTrInverse(Matrix A)
+        {
+            Matrix M = new Matrix(A.rows, A.cols);
+
+            for (int i = 0; i < A.rows; i++)
+            {
+                M.values[i,i] = 1 / A.values[i, i];
+                for (int j = 0; j < i; j++)
+                {
+                    for (int k = 0; k < i; k++)
+                    {
+                        M.values[i, j] += M.values[k, j] * A.values[i, k];
+                    }
+                    M.values[i, j] *= (-1 / A.values[i, i]);
+                }
+            }
+
+            return M;
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Matrix A = new Matrix(3, 3);    //5x1 + 3x2 + x3 = 14   x1 = 1
+            A.values = new double[3,3]{     //-2x1 + x2 - 3x3 = -9  x2 = 2
+                {5, 3, 1},                  // x1 + 7x2 - 2x3 = 9   x3 = 3
+                {-2, 1, -3},
+                {1, 7, -2}};
+            Matrix b = new Matrix(3, 1);
+            b.values = new double[3, 1] { { 14 }, { -9 }, { 9 } };
+            Matrix L = A.ToLowTr();
+            Matrix M = LowTrInverse(L);
+            MessageBox.Show(UpperRelax(A, b, 0.00000001, 1000000).ToString());
+            //MessageBox.Show(JacobiMethod(A, b, 0.00000001, 1000000).ToString());   
+        }
+    }
+}
---
+++ WpfApplication1/Matrix.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class Matrix
+    {
+        public int rows;
+        public int cols;
+        public double[,] values;
+
+
+        public Matrix(int rowCount, int colCount)
+        {
+            rows = rowCount;
+            cols = colCount;
+            values = new double[rows, cols];
+        }
+
+
+        public Matrix Copy()
+        {
+            Matrix res = new Matrix(this.rows, this.cols);
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    res.values[i, j] = this.values[i, j];
+                }
+            }
+
+            return res;
+        }
+
+
+        public void ToIdentityMatrix()
+        {
+            if (this.rows != this.cols)
+                throw new Exception("Matrix is not square");
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    if (i == j)
+                        this.values[i, j] = 1;
+                    else
+                        this.values[i, j] = 0;
+                }
+            }
+        }
+
+
+        public static Matrix operator +(Matrix A, Matrix B)
+        {
+            if ((A.rows != B.rows) || (A.cols != B.cols))
+                throw new Exception("Matrices' sizes are not equal");
+            Matrix C = new Matrix(A.rows, B.cols);
+
+            for (int i = 0; i < A.rows; i++)
+            {
+                for (int j = 0; j < A.cols; j++)
+                {
+                    C.values[i, j] = A.values[i, j] + B.values[i, j];
+                }
+            }
+
+            return C;
+        }
+
+
+        public static Matrix operator -(Matrix A, Matrix B)
+        {
+            if ((A.rows != B.rows) || (A.cols != B.cols))
+                throw new Exception("Matrices' sizes are not equal");
+            Matrix C = new Matrix(A.rows, B.cols);
+
+            for (int i = 0; i < A.rows; i++)
+            {
+                for (int j = 0; j < A.cols; j++)
+                {
+                    C.values[i, j] = A.values[i, j] - B.values[i, j];
+                }
+            }
+
+            return C;
+        }
+
+
+        public static Matrix operator *(double t, Matrix A)
+        {
+            Matrix res = new Matrix(A.rows, A.cols);
+
+            for (int i = 0; i < A.rows; i++)
+            {
+                for (int j = 0; j < A.cols; j++)
+                {
+                    res.values[i, j] = t * A.values[i, j];
+                }
+            }
+
+            return res;
+        }
+
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            if (A.cols != B.rows)
+                throw new Exception("Matrices' sizes are inappropriate");
+            Matrix C = new Matrix(A.rows, B.cols);
+
+            for (int i = 0; i < A.rows; i++)
+            {
+                for (int j = 0; j < B.cols; j++)
+                {
+                    for (int k = 0; k < A.cols; k++)
+                    {
+                        C.values[i, j] += A.values[i, k] * B.values[k, j];
+                    }
+                }
+            }
+
+            return C;
+        }
+
+
+        public Matrix Transpose()
+        {
+            Matrix res = new Matrix(this.cols, this.rows);
+
+            for (int i = 0; i < this.cols; i++)
+            {
+                for (int j = 0; j < this.rows; j++)
+                {
+                    res.values[i, j] = this.values[j, i];
+                }
+            }
+
+            return res;
+        }
+
+
+        public double Dot(Matrix b)
+        {
+            if ((this.cols != 1) || (b.cols != 1))
+                throw new Exception("Operands are not vectors");
+            if (this.rows != b.rows)
+                throw new Exception("Vectors' lengths are not equal");
+            double sum = 0;
+
+            for (int i = 0; i < this.rows; i++)
+                sum += this.values[i, 0] * this.values[i, 0];
+
+            return sum;
+        }
+
+
+        public double Norm()
+        {
+            double maxSum = 0;
+            double rowSum;
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                rowSum = 0;
+                for (int j = 0; j < this.cols; j++)
+                {
+                    rowSum += Math.Abs(this.values[i, j]);
+                }
+                if (rowSum > maxSum)
+                    maxSum = rowSum;
+            }
+
+            return maxSum;
+        }
+
+
+        public override string ToString()
+        {
+            string result = "";
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    result += this.values[i, j] + " ";
+                }
+                result += Environment.NewLine;
+            }
+
+            return result;
+        }
+
+
+        public Matrix ToLowTr() //В методе верхней релаксации не просто нижняя треугольная, а M = L + t * D !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        {
+            if (this.rows != this.cols)
+                throw new Exception("Matrix is not square");
+
+            Matrix L = new Matrix(this.rows, this.cols);
+
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    if (i >= j)
+                        L.values[i, j] = this.values[i, j];
+                }
+            }
+
+            return L;
+        }
+    }
+}
